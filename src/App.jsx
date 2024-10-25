7a9fe315e87c7912d4a0f0e87304cc4a1ee8e012
SaiMohanSattari-AidenAI <saimohan.sattari03@aidenai.com>
2024-10-25 20:05:11 +0530

    Basic Folder Structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomePage from './pages/homePage/HomePage'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
